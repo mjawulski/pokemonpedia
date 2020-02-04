e4aaac235482e406b287cd8676e8110229667b5f
Michał Jawulski <mjawulski@gmail.com>
2020-02-04 21:56:35 +0100

    pokemon type in polish

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -22,9 +22,52 @@ export class PokemonCardComponent implements OnInit {
     this.httpClient.get<any>(this.cardData.url).subscribe(response => {
       this.pokemon = {
         name: response.name,
-        type: response.types[0].type.name,
+        type: this.translateTypeName(response.types[0].type.name),
         imageUrl: response.sprites.front_default
       };
     });
+  }
+
+  translateTypeName(type: string): string {
+    switch (type) {
+      case "normal":
+        return "normalny";
+      case "fire":
+        return "ognisty";
+      case "water":
+        return "wodnisty";
+      case "electric":
+        return "elektryczny";
+      case "grass":
+        return "trawiasty";
+      case "ice":
+        return "lodowy";
+      case "fighting":
+        return "walczący";
+      case "poison":
+        return "trujący";
+      case "ground":
+        return "ziemnisty";
+      case "flying":
+        return "latający";
+      case "psychic":
+        return "psychiczny";
+      case "bug":
+        return "robak";
+      case "rock":
+        return "skalny";
+      case "ghost":
+        return "duch";
+      case "dragon":
+        return "smok";
+      case "dark":
+        return "mrok";
+      case "steel":
+        return "stalowy";
+      case "fairy":
+        return "baśniowy";
+      default:
+        return "typ nieobsługiwany";
+    }
   }
 }
